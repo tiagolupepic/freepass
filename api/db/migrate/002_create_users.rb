dd99cd522282e2cc76d11ebedfd992c47e69b960
Tiago Lupepic <tiagolpc@gmail.com>
2016-07-11 23:25:06 -0300

    Update with token reset fields

--- api/db/migrate/002_create_users.rb
+++ api/db/migrate/002_create_users.rb
@@ -3,7 +3,8 @@ class CreateUsers < ActiveRecord::Migration
     create_table :users, id: :uuid do |t|
       t.string :full_name, :phone, :address, :email
       t.string :password, :cpf
-      t.string :role, :state
+      t.string :role, :state, :reset_password_token
+      t.datetime :reset_password_sent_at
 
       t.timestamps null: false
     end
