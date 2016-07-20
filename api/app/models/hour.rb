e71e8545bfe6110d65d266211eeb0aac1c17a235
Tiago Lupepic <tiagolpc@gmail.com>
2016-07-20 12:32:52 -0300

    Add model hour and hour specs

---
+++ api/app/models/hour.rb
@@ -0,0 +1,4 @@
+class Hour < ActiveRecord::Base
+  store_accessor :days, :sunday, :monday, :tuesday, :wednesday, :thursday, :friday, :saturday
+  validates :name, :start_at, :end_at, presence: true
+end
---
+++ api/spec/models/hour_spec.rb
@@ -0,0 +1,17 @@
+require 'spec_helper'
+
+RSpec.describe Hour, type: :model do
+  it { should validate_presence_of :name }
+  it { should validate_presence_of :start_at }
+  it { should validate_presence_of :end_at }
+
+  context '.days' do
+    it { should respond_to :sunday }
+    it { should respond_to :monday }
+    it { should respond_to :tuesday }
+    it { should respond_to :wednesday }
+    it { should respond_to :thursday }
+    it { should respond_to :friday }
+    it { should respond_to :saturday }
+  end
+end
