003b58f9dd1ff3e058431fe9ea6dc27666cd2393
Tiago Lupepic <tiagolpc@gmail.com>
2016-10-29 11:25:28 -0200

    Add paginate method to RequestHelper

--- api/app/helpers/request_helper.rb
+++ api/app/helpers/request_helper.rb
@@ -16,4 +16,11 @@ module RequestHelper
   def errors_or_object(object)
     object.valid? ? object : halt_request(422, {error: object.errors.full_messages })
   end
+
+  def paginate(object)
+    response.headers["X-Total-Pages"] = object.total_pages.to_s
+    response.headers["X-Total-Count"] = object.total_entries.to_s
+    response.headers["X-Per-Page"]    = "10"
+    object
+  end
 end
