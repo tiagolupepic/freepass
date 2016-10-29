449c9d15b2fb2549fc5531a66d764c8929f85853
Tiago Lupepic <tiagolpc@gmail.com>
2016-10-29 14:51:04 -0200

    Add resource to CORS

--- api/app/api.rb
+++ api/app/api.rb
@@ -4,6 +4,7 @@ class Api < Roda
   use Rack::Cors do
     allow do
       origins '*'
+      resource '*', headers: :any, methods: [:get, :post, :delete, :put, :patch, :options, :head]
     end
   end
 
