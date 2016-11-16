0332184fcabb97f98bb2a6ddbc8e8b8046f1d6b6
Tiago Lupepic <tiagolpc@gmail.com>
2016-11-16 00:14:16 -0200

    Fix JSONParser to fetch correct data

--- admin/lib/freepass/middlewares/json_parser.rb
+++ admin/lib/freepass/middlewares/json_parser.rb
@@ -2,9 +2,21 @@ class JSONParser < Faraday::Response::Middleware
   def parse(body)
     json = MultiJson.load(body, symbolize_keys: true)
     {
-      data: json.delete(:errors),
-      errors: json.try(:errors) || {},
-      metadata: json.try(:metadata) || {}
+      errors: errors(json),
+      data: data(json),
+      metadata: {}
     }
+  end
+
+  private
+
+  def errors(json)
+    return {} if json.kind_of? Array or not json.keys.include?(:errors)
+    json[:errors]
+  end
+
+  def data(json)
+    return json if json.kind_of? Array
+    json.except(:errors)
   end
 end
