93770bddbd34a6363f7286e5893d18a192c83511
Tiago Lupepic <tiagolpc@gmail.com>
2016-11-19 00:11:27 -0200

    Rollback assets

--- admin/apps/web/application.rb
+++ admin/apps/web/application.rb
@@ -324,7 +324,7 @@ module Web
         # Don't compile static assets in production mode (eg. Sass, ES6)
         #
         # See: http://www.rubydoc.info/gems/hanami-assets#Configuration
-        compile true
+        compile false
 
         # Use digest file name for asset paths
         #
