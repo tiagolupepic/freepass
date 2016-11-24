a145a4a88a88137c6c4ab2250d3fced99fc12d8a
Tiago Lupepic <tiagolpc@gmail.com>
2016-11-24 01:03:54 -0200

    Add AdminAuthenticator with object and valid? methods

--- api/app/authenticators/admin.rb
+++ api/app/authenticators/admin.rb
@@ -1,3 +1,33 @@
 class AdminAuthenticator
   # this check if exist password with user and his is admin
+
+  attr_reader :password
+
+  def initialize(password)
+    @password = password
+  end
+
+  def valid?
+    return false unless password
+    user.present? and user_admin?
+  end
+
+  def object
+    return unless password
+    user
+  end
+
+  private
+
+  def user_admin?
+    user.role == "admin"
+  end
+
+  def user
+    @user ||= User.find_by password: digest_password
+  end
+
+  def digest_password
+    Digest::SHA1.hexdigest(password)
+  end
 end
--- api/spec/authenticators/admin_spec.rb
+++ api/spec/authenticators/admin_spec.rb
@@ -1,4 +1,41 @@
 require 'spec_helper'
 
 RSpec.describe AdminAuthenticator do
+  let(:password) { nil }
+  let(:role)     { :user }
+
+  subject { described_class.new(password) }
+
+  it 'should not be valid' do
+    expect(subject).to_not be_valid
+  end
+
+  context 'with valid user' do
+    let(:password) { '123456' }
+    let!(:user)    { create :user, password: password, role: role }
+
+    it 'should be invalid' do
+      expect(subject).to_not be_valid
+    end
+
+    context 'with role admin' do
+      let(:role) { :admin }
+
+      it 'should be valid' do
+        expect(subject).to be_valid
+      end
+
+      describe '#object' do
+        it 'should receive object' do
+          expect(subject.object).to eq user
+        end
+      end
+    end
+  end
+
+  describe '#object' do
+    it 'should return nil' do
+      expect(subject.object).to be_nil
+    end
+  end
 end
