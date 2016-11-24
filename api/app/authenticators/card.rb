777ace2862413b9f6547904768336bf5d76778e9
Tiago Lupepic <tiagolpc@gmail.com>
2016-11-24 00:30:39 -0200

    Implement first version ot CardAuthenticator

--- api/app/authenticators/card.rb
+++ api/app/authenticators/card.rb
@@ -4,6 +4,70 @@ class CardAuthenticator
   # check if card is strict to access same hours
   # check if card have holiday permissions
   # check if user is blocked
-  #
-  # generate events
+  WDAYS = { sunday: 0, monday: 1, tuesday: 2, wednesday: 3, thursday: 4, friday: 5, saturday: 6 }
+
+  attr_accessor :number
+
+  def initialize(number)
+    @number = number
+  end
+
+  def valid?
+    return false unless number
+    card.present? and valid_period? and valid_hour? and valid_holiday?
+  end
+
+  private
+
+  def valid_period?
+    return true if user_periods.empty?
+    user_periods.include? today.to_date
+  end
+
+  def valid_hour?
+    return true if user.hours.empty?
+    user_hours.any?
+  end
+
+  def valid_holiday?
+    return true unless holiday
+    user.holidays.include? holiday
+  end
+
+  def wday_valid?(h)
+    days = h.days.select { |_,d| d == "true" }
+    return true if days.empty?
+    days.keys.include? WDAYS.key(today.wday).to_s
+  end
+
+  def user_periods
+    @periods ||= user.periods.collect { |period| (period.start_at.to_date..period.end_at.to_date).to_a }.flatten.uniq
+  end
+
+  def user_hours
+    user.periods.select do |period|
+      next unless (period.start_at.to_date..period.end_at.to_date).to_a.include? today.to_date
+
+      period.hours.empty? or
+      period.hours.any? do |h|
+        today >= today.change(hour: h.start_at.hour, min: h.start_at.min) and today <= today.change(hour: h.end_at.hour, min: h.end_at.min) and wday_valid?(h)
+      end
+    end
+  end
+
+  def holiday
+    @holiday ||= Holiday.find_by happen_at: today.to_date
+  end
+
+  def card
+    @card ||= Card.find_by number: number
+  end
+
+  def user
+    card.user
+  end
+
+  def today
+    Time.now
+  end
 end
--- api/spec/authenticators/card_spec.rb
+++ api/spec/authenticators/card_spec.rb
@@ -1,4 +1,178 @@
 require 'spec_helper'
 
 RSpec.describe CardAuthenticator do
+  let(:number) { nil }
+
+  subject { described_class.new(number) }
+
+  describe '#valid?' do
+    it 'should not be valid' do
+      expect(subject).to_not be_valid
+    end
+
+    context 'with invalid card' do
+      let(:number) { 'ABC123' }
+
+      it 'should not be valid' do
+        expect(subject).to_not be_valid
+      end
+    end
+
+    context 'with valid card' do
+      let(:card)   { create :card }
+      let(:number) { card.number }
+
+      it 'should be valid' do
+        expect(subject).to be_valid
+      end
+
+      context 'with one period' do
+        let(:date)    { Time.parse('Nov, 3 2016 11:00:00') }
+        let(:periods) { [build(:period, start_at: Time.parse('Nov, 2 2016'), end_at: Time.parse('Nov, 5 2016'), hours: hours)] }
+        let(:user)    { build :user, periods: periods }
+        let(:card)    { create :card, user: user }
+        let(:hours)   { [] }
+        let(:days)    { {} }
+
+        before { Timecop.freeze date }
+        after  { Timecop.return }
+
+        it 'should be valid' do
+          expect(subject).to be_valid
+        end
+
+        context 'when date not belongs to period' do
+          let(:date) { Time.parse('Nov, 10 2016') }
+
+          it 'should not be valid' do
+            expect(subject).to_not be_valid
+          end
+        end
+
+        context 'and more periods' do
+          let(:periods) { [
+            build(:period, start_at: Time.parse('Nov, 2 2016'),  end_at: Time.parse('Nov, 5 2016'),  hours: hours),
+            build(:period, start_at: Time.parse('Nov, 10 2016'), end_at: Time.parse('Nov, 20 2016')),
+          ] }
+
+          it 'should be valid' do
+            expect(subject).to be_valid
+          end
+
+          context 'when date not belongs to period' do
+            let(:date) { Time.parse('Nov, 22 2016') }
+
+            it 'should not be valid' do
+              expect(subject).to_not be_valid
+            end
+          end
+
+          context 'with multiple hours' do
+            let(:hours) { [
+              create(:hour, start_at: Time.parse('10:00:00'), end_at: Time.parse('12:00:00'), days: days),
+              create(:hour, start_at: Time.parse('14:00:00'), end_at: Time.parse('18:00:00'))
+            ] }
+
+            it 'should be valid' do
+              expect(subject).to be_valid
+            end
+
+            context 'when date belong to afternoon' do
+              let(:date) { Time.parse('Nov, 3 2016 15:30:00') }
+
+              it 'should be valid' do
+                expect(subject).to be_valid
+              end
+            end
+
+            context 'when date is at night' do
+              let(:date) { Time.parse('Nov, 3 2016 18:30:00') }
+
+              it 'should be valid' do
+                expect(subject).to_not be_valid
+              end
+            end
+
+            context 'when date not belongs to hours' do
+              let(:date) { Time.parse('Nov, 12 2016') }
+
+              it 'should not be valid' do
+                expect(subject).to be_valid
+              end
+            end
+
+            context 'with days' do
+              let(:days) { { monday: true, thursday: true } }
+
+              it 'should be valid' do
+                expect(subject).to be_valid
+              end
+
+              context 'with monday' do
+                let(:days) { { monday: true, thursday: false } }
+
+                it 'should not be valid' do
+                  expect(subject).to_not be_valid
+                end
+              end
+            end
+          end
+        end
+
+        context 'with one hour' do
+          let(:hours) { [create(:hour, start_at: Time.parse('10:00:00'), end_at: Time.parse('12:00:00'), days: days)] }
+
+          it 'should be valid' do
+            expect(subject).to be_valid
+          end
+
+          context 'when hours not belongs' do
+            let(:date) { Time.parse('Nov, 3 2016 12:01:00') }
+
+            it 'should not be valid' do
+              expect(subject).to_not be_valid
+            end
+          end
+
+          context 'with days' do
+            let(:days) { { monday: true, thursday: true } }
+
+            it 'should be valid' do
+              expect(subject).to be_valid
+            end
+
+            context 'with monday' do
+              let(:days) { { monday: true, thursday: false } }
+
+              it 'should not be valid' do
+                expect(subject).to_not be_valid
+              end
+            end
+          end
+        end
+      end
+
+      context 'with holidays' do
+        let(:date)     { Time.parse('Nov, 3 2016 11:00:00') }
+        let!(:holiday) { create :holiday, happen_at: date }
+        let(:user)     { build :user }
+        let(:card)     { create :card, user: user }
+
+        before { Timecop.freeze date }
+        after  { Timecop.return }
+
+        it 'should not be valid' do
+          expect(subject).to_not be_valid
+        end
+
+        context 'and user have holiday' do
+          let(:user) { build :user, holidays: [holiday] }
+
+          it 'should be valid' do
+            expect(subject).to be_valid
+          end
+        end
+      end
+    end
+  end
 end
