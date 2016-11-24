c4bcf8f5f0febbe0e547d22630a902e94c1d96c4
Tiago Lupepic <tiagolpc@gmail.com>
2016-11-23 23:40:00 -0200

    Remove validation of days on Hour model

--- api/app/models/hour.rb
+++ api/app/models/hour.rb
@@ -2,5 +2,5 @@ class Hour < ActiveRecord::Base
   store_accessor :days, :sunday, :monday, :tuesday, :wednesday, :thursday, :friday, :saturday
   has_and_belongs_to_many :periods
 
-  validates :name, :start_at, :end_at, :days, presence: true
+  validates :name, :start_at, :end_at, presence: true
 end
--- api/spec/models/hour_spec.rb
+++ api/spec/models/hour_spec.rb
@@ -4,7 +4,6 @@ RSpec.describe Hour, type: :model do
   it { should validate_presence_of :name }
   it { should validate_presence_of :start_at }
   it { should validate_presence_of :end_at }
-  it { should validate_presence_of :days }
 
   it { should have_and_belong_to_many :periods }
 
