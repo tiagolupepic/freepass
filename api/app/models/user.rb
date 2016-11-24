b68596c35d9cba6d9f710afb4c4d39267144d856
Tiago Lupepic <tiagolpc@gmail.com>
2016-11-24 00:41:57 -0200

    Implement state_machine to user

--- api/app/models/user.rb
+++ api/app/models/user.rb
@@ -7,4 +7,14 @@ class User < ActiveRecord::Base
   has_many :hours, through: :periods
 
   validates :full_name, :email, :cpf, presence: true
+
+  state_machine initial: :requested do
+    event :activate do
+      transition requested: :activated
+    end
+
+    event :deactivate do
+      transition [:activated, :requested] => :deactivated
+    end
+  end
 end
--- api/spec/models/user_spec.rb
+++ api/spec/models/user_spec.rb
@@ -12,4 +12,42 @@ RSpec.describe User, type: :model do
   it { should have_many :hours }
   it { should have_and_belong_to_many :periods }
   it { should have_and_belong_to_many :holidays }
+
+  describe 'state machine' do
+    let(:user) { create :user }
+
+    it 'should start' do
+      expect(user.requested?).to be_truthy
+    end
+
+    describe '#activate' do
+      before do
+        user.activate!
+      end
+
+      it 'should change state' do
+        expect(user.activated?).to be_truthy
+      end
+
+      describe '#deactivate' do
+        before do
+          user.deactivate!
+        end
+
+        it 'should change state' do
+          expect(user.deactivated?).to be_truthy
+        end
+      end
+    end
+
+    describe '#deactivate' do
+      before do
+        user.deactivate!
+      end
+
+      it 'should change state' do
+        expect(user.deactivated?).to be_truthy
+      end
+    end
+  end
 end
