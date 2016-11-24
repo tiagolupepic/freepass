6044b8685337fe2ddc68c57f70ee2e33da21bb93
Tiago Lupepic <tiagolpc@gmail.com>
2016-11-23 23:43:43 -0200

    Remove monday from Hour factory girl

--- api/spec/factories/hours.rb
+++ api/spec/factories/hours.rb
@@ -3,6 +3,6 @@ FactoryGirl.define do
     name     'Interval'
     start_at { Time.now.strftime('%H:%M:%S') }
     end_at   { Time.now.strftime('%H:%M:%S') }
-    days     { { monday: true } }
+    days     { {} }
   end
 end
