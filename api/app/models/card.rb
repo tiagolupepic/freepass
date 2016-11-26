26127f034410174b2ff30432ea8af209c5afe793
Tiago Lupepic <tiagolpc@gmail.com>
2016-11-26 19:01:44 -0200

    Add search tpo Card

--- api/app/models/card.rb
+++ api/app/models/card.rb
@@ -1,4 +1,7 @@
 class Card < ActiveRecord::Base
+  include PgSearch
+  pg_search_scope :search, :against => [:number]
+
   belongs_to :user
 
   validates :user, :number, presence: true
--- api/spec/models/card_spec.rb
+++ api/spec/models/card_spec.rb
@@ -1,7 +1,15 @@
 require 'spec_helper'
 
 RSpec.describe Card, type: :model do
+  it { expect(described_class.ancestors).to include(PgSearch) }
+
   it { should validate_presence_of(:number) }
   it { should validate_presence_of(:user) }
   it { should belong_to :user }
+
+  describe '.search' do
+    it 'should have method' do
+      expect(Card).to respond_to(:search)
+    end
+  end
 end
