a195045c9ee783ed92e1e8b96a7505a81832da24
Tiago Lupepic <tiagolpc@gmail.com>
2016-11-26 15:20:22 -0200

    Add methods to use filter params

--- api/app/helpers/filter_params.rb
+++ api/app/helpers/filter_params.rb
@@ -1,4 +1,20 @@
 module FilterParams
+  def card_params
+    @card_params ||= CardFilterParams.new(params).result
+  end
+
+  def holiday_params
+    @holiday_params ||= HolidayFilterParams.new(params).result
+  end
+
+  def hour_params
+    @hour_params ||= HourFilterParams.new(params).result
+  end
+
+  def period_params
+    @period_params ||= PeriodFilterParams.new(params).result
+  end
+
   def user_params
     @user_params ||= UserFilterParams.new(params).result
   end
