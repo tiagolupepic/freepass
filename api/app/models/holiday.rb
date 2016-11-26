33e3d2f4190e6058ade9a7909d7e6000810ff721
Tiago Lupepic <tiagolpc@gmail.com>
2016-11-26 19:08:27 -0200

    Add PgSearch to Holiday

--- api/app/models/holiday.rb
+++ api/app/models/holiday.rb
@@ -1,4 +1,7 @@
 class Holiday < ActiveRecord::Base
+  include PgSearch
+  pg_search_scope :search, :against => [:name]
+
   has_and_belongs_to_many :users
 
   validates :name, :happen_at, presence: true
--- api/spec/models/holiday_spec.rb
+++ api/spec/models/holiday_spec.rb
@@ -1,8 +1,16 @@
 require 'spec_helper'
 
 RSpec.describe Holiday, type: :model do
+  it { expect(described_class.ancestors).to include(PgSearch) }
+
   it { should validate_presence_of :name }
   it { should validate_presence_of :happen_at }
 
   it { should have_and_belong_to_many :users }
+
+  describe '.search' do
+    it 'should have method' do
+      expect(Holiday).to respond_to(:search)
+    end
+  end
 end
