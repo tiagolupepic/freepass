f5cda7654e2a8781cc0cef0d1571d8fa81c337ea
Tiago Lupepic <tiagolpc@gmail.com>
2016-12-03 15:48:32 -0200

    Add dependency to destroy cards on relationship

--- api/app/models/user.rb
+++ api/app/models/user.rb
@@ -8,7 +8,7 @@ class User < ActiveRecord::Base
                     :tsearch => { prefix: true }
                   }
 
-  has_many :cards
+  has_many :cards, dependent: :destroy
   has_and_belongs_to_many :periods
   has_and_belongs_to_many :holidays
   has_many :hours, through: :periods
